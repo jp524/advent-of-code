845fd9df988dfd52a7e0e47d3572fedb2ebd092d
JP <85654561+jp524@users.noreply.github.com>
2023-04-25 17:40:20 -0400

    Solve day 1 part 2

--- 2021/01/01.rb
+++ 2021/01/01.rb
@@ -3,18 +3,28 @@ def number_increase(array)
 
   (0...array.length - 1).each do |i|
     count += 1 if array[i + 1] > array[i]
-    count
   end
 
   count
 end
 
-def day_01
-  file = File.open('01_input.txt')
+def sliding_window_increase(array)
+  count = 0
+
+  (0...array.length - 3).each do |i|
+    count += 1 if array[i + 1..i + 3].sum > array[i..i + 2].sum
+  end
+
+  count
+end
+
+def day01
+  file = File.open('01/01_input.txt')
   input_string = file.readlines.map(&:chomp)
   input_int = input_string.map(&:to_i)
 
   p number_increase(input_int)
+  p sliding_window_increase(input_int)
 end
 
-day_01
+day01
--- 2021/01/01_spec.rb
+++ 2021/01/01_spec.rb
@@ -1,15 +1,29 @@
-require_relative "./01"
+require_relative './01'
 
-describe "Day 1" do
-  it "returns 7 with [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]" do
+describe 'Day 1 Part 1' do
+  it 'returns 7 with [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]' do
     expect(number_increase([199, 200, 208, 210, 200, 207, 240, 269, 260, 263])).to eq(7)
   end
 
-  it "returns 4 with [1, 2, 5, 3, 4, 7]" do
+  it 'returns 4 with [1, 2, 5, 3, 4, 7]' do
     expect(number_increase([1, 2, 5, 3, 4, 7])).to eq(4)
   end
 
-  it "returns 0 with [10, 1]" do
+  it 'returns 0 with [10, 1]' do
     expect(number_increase([10, 1])).to eq(0)
+  end
+
+  describe 'Day 1 Part 2' do
+    it 'returns 5 with [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]' do
+      expect(sliding_window_increase([199, 200, 208, 210, 200, 207, 240, 269, 260, 263])).to eq(5)
+    end
+
+    it 'returns 1 with [1, 2, 3, 4]' do
+      expect(sliding_window_increase([1, 2, 3, 4])).to eq(1)
+    end
+
+    it 'returns 0 with [2, 3, 4, 1]' do
+      expect(sliding_window_increase([2, 3, 4, 1])).to eq(0)
+    end
   end
 end
