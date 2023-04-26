64b42f5b8e1903d32d045748a591c630774d5eaf
JP <85654561+jp524@users.noreply.github.com>
2023-04-25 22:21:07 -0400

    Solve day 2 part 2

--- 2021/02/02.rb
+++ 2021/02/02.rb
@@ -14,6 +14,23 @@ def move_submarine(instructions)
   position
 end
 
+def move_submarine_with_aim(instructions)
+  position = [0, 0, 0]
+  # [horizontal, depth, aim]
+  instructions.each do |direction, value|
+    case direction
+    when 'forward'
+      position[0] += value.to_i
+      position[1] += position[2] * value.to_i
+    when 'down'
+      position[2] += value.to_i
+    when 'up'
+      position[2] -= value.to_i
+    end
+  end
+  position
+end
+
 def day02
   file = File.open('02/02_input.txt')
   input = file.readlines.map(&:chomp)
@@ -23,6 +40,9 @@ def day02
 
   final_position = move_submarine(instructions)
   p final_position.inject(:*)
+
+  final_position_with_aim = move_submarine_with_aim(instructions)
+  p final_position_with_aim[0] * final_position_with_aim[1]
 end
 
 day02
--- 2021/02/02_spec.rb
+++ 2021/02/02_spec.rb
@@ -16,3 +16,20 @@ describe 'Day 2 Part 1' do
                           ])).to eq([15, 10])
   end
 end
+
+describe 'Day 2 Part 2' do
+  it 'returns [5, 0, 5] with [%w[forward 5], %w[down 5]]' do
+    expect(move_submarine_with_aim([%w[forward 5], %w[down 5]])).to eq([5, 0, 5])
+  end
+
+  it 'returns [15, 60, 10] with [%w[forward 5], %w[down 5], %w[up 3], %w[down 8], %w[forward 2]]' do
+    expect(move_submarine_with_aim([
+                                     %w[forward 5],
+                                     %w[down 5],
+                                     %w[forward 8],
+                                     %w[up 3],
+                                     %w[down 8],
+                                     %w[forward 2]
+                                   ])).to eq([15, 60, 10])
+  end
+end
