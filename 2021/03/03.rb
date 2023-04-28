aa7debdc0831d044959c0b4a088ff732b391ceac
JP <85654561+jp524@users.noreply.github.com>
2023-04-27 23:25:08 -0400

    Solve day 3 part 2

--- 2021/03/03.rb
+++ 2021/03/03.rb
@@ -1,3 +1,4 @@
+# Part 1
 def convert_to_grid(string_input)
   string_input.map do |line|
     line.split(//).map(&:to_i)
@@ -39,13 +40,71 @@ def power_consumption(grid)
   multiply_binary_arrays(gamma_array, epsilon_array)
 end
 
+# Part 2
+def tally_binaries(array)
+  array.tally
+end
+
+def num_to_keep(array, rounding_value)
+  binary_tally = tally_binaries(array)
+
+  case rounding_value
+  when 1
+    if binary_tally[0] == binary_tally[1]
+      rounding_value
+    else
+      binary_tally.key(binary_tally.values.max)
+    end
+  when 0
+    if binary_tally[0] == binary_tally[1]
+      rounding_value
+    else
+      binary_tally.key(binary_tally.values.min)
+    end
+  end
+end
+
+def filter_grid_at_column(grid, position_index, num_to_keep)
+  grid.select! { |row| row[position_index] == num_to_keep }
+  grid
+end
+
+def filter_grid_with_param(grid, rounding_value)
+  position_index = 0
+
+  grid.length.times do
+    return grid.first if grid.length == 1
+
+    transposed_grid = grid.transpose
+    column_to_filter = transposed_grid[position_index]
+
+    num_to_keep = num_to_keep(column_to_filter, rounding_value)
+    grid = filter_grid_at_column(grid, position_index, num_to_keep)
+    position_index += 1
+  end
+end
+
+def oxygen_generator_array(grid)
+  filter_grid_with_param(grid, 1)
+end
+
+def co2_scrubber_rating(grid)
+  filter_grid_with_param(grid, 0)
+end
+
+def life_support_rating(grid)
+  multiply_binary_arrays(oxygen_generator_array(Marshal.load(Marshal.dump(grid))), co2_scrubber_rating(Marshal.load(Marshal.dump(grid))))
+end
+
+# Common to both parts
 def day03
   file = File.open('03/03_input.txt')
   input = file.readlines.map(&:chomp)
-  p input
-  grid = convert_to_grid(input)
 
+  grid = convert_to_grid(input)
   p power_consumption(grid)
+
+  p life_support_rating(grid)
 end
 
 day03
--- 2021/03/03_spec.rb
+++ 2021/03/03_spec.rb
@@ -32,3 +32,78 @@ describe 'Day 3 Part 1' do
     expect(power_consumption(grid)).to eq(198)
   end
 end
+
+describe 'Day 3 Part 2' do
+  it 'returns a 1 when given a rounding_value of 1' do
+    expect(num_to_keep([0, 1], 1)).to eq(1)
+  end
+
+  it 'returns a 1 when given a rounding_value of 0' do
+    expect(num_to_keep([0, 1], 0)).to eq(0)
+  end
+
+  it 'returns a filtered grid based on position and number parameters' do
+    expect(filter_grid_at_column([
+                                   [0, 0, 1, 0, 0],
+                                   [1, 1, 1, 1, 0],
+                                   [1, 0, 1, 1, 0]
+                                 ], 0, 1)).to eq([[1, 1, 1, 1, 0], [1, 0, 1, 1, 0]])
+  end
+
+  it 'returns the correct oxygen generator rating' do
+    input = %w[
+      00100
+      11110
+      10110
+      10111
+      10101
+      01111
+      00111
+      11100
+      10000
+      11001
+      00010
+      01010
+    ]
+    grid = convert_to_grid(input)
+    expect(oxygen_generator_array(grid)).to eq([1, 0, 1, 1, 1])
+  end
+
+  it 'returns the correct CO2 scrubber rating' do
+    input = %w[
+      00100
+      11110
+      10110
+      10111
+      10101
+      01111
+      00111
+      11100
+      10000
+      11001
+      00010
+      01010
+    ]
+    grid = convert_to_grid(input)
+    expect(co2_scrubber_rating(grid)).to eq([0, 1, 0, 1, 0])
+  end
+
+  it 'returns 230 when given the example as input' do
+    input = %w[
+      00100
+      11110
+      10110
+      10111
+      10101
+      01111
+      00111
+      11100
+      10000
+      11001
+      00010
+      01010
+    ]
+    grid = convert_to_grid(input)
+    expect(life_support_rating(grid)).to eq(230)
+  end
+end
